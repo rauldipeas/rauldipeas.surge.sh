944db25ec5573c61f1c0d5cfbba95b5940e49072
Raul Dipeas <rauldipeas@disroot.org>
2022-06-28 01:28:27 -0300

    ^session-qr-code ^js

---
+++ public/assets/js/load.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.body.classList.remove('is-preload');
+}
+window.ontouchmove = function () {
+    return false;
+}
+window.onorientationchange = function () {
+    document.body.scrollTop = 0;
+}
