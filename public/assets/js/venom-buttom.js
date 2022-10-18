093b18d145e94fcd1c1d7cd1ca32145a414df981
Raul Dipeas <rauldipeas@disroot.org>
2022-10-18 13:43:42 -0300

    +venom-button

---
+++ public/assets/js/venom-buttom.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('#venom-button').venomButton({
+        phone: '5511954421574',
+        chatMessage: 'Olá 👋<br><br>Como posso te ajudar?',
+        showPopup: true,
+        avatar: 'assets/images/avatar.jpg',
+        position: "right",
+        headerColor: '#033E8C',
+        buttonColor: '#033E8C',
+        linkButton: false,
+        showOnIE: false,
+        nameClient: "Raul Dipeas",
+        headerTitle: 'Técnico em informática, músico e cinéfilo.'
+    });
+});
