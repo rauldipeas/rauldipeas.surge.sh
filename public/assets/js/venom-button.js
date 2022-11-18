785a5e0011046aefc733c97faae10524e59000c1
Raul Dipeas <rauldipeas@disroot.org>
2022-11-18 11:12:28 -0300

    ^venom-button

--- public/assets/js/venom-button.js
+++ public/assets/js/venom-button.js
@@ -2,7 +2,7 @@ $(function () {
     $('#venom-button').venomButton({
         phone: '5511954421574',
         chatMessage: 'Olá 👋<br><br>Como posso te ajudar?',
-        buttonImage: 'https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/whatsapp.svg',
+        buttonImage: 'https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/48x48/apps/whatsapp.svg',
         showPopup: true,
         avatar: 'assets/images/avatar.jpg',
         position: "right",
