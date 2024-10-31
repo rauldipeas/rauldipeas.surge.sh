dfad4de84e718c899f3ff7d21278de20f01e5222
Raul Dipeas <rauldipeas@disroot.org>
2024-10-31 04:24:30 -0300

    ^surge

--- scripts/actions-cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-# Actions cleanup
-PROJECT='rauldipeas/rauldipeas.surge.sh'
-KEEP=1
-gh api\
-	repos/"$PROJECT"/actions/runs\
-	--paginate -q '.workflow_runs[]|select(.head_branch != "any")|"\(.id)"'|tail -n+$((KEEP+1))\
-    |xargs -n1 -I % gh api repos/"$PROJECT"/actions/runs/% -X DELETE
